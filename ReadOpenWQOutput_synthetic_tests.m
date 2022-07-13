52e10b3bdde476702d9f3a241ea46aab6eb9bfa2
Diogo Costa <diogo.costa@usask.ca>
2022-07-13 19:36:39 +0100

    automating processing of results for multiple runs and models for generation of mat files

--- ReadOpenWQOutput_synthetic_tests.m
+++ ReadOpenWQOutput_synthetic_tests.m
@@ -1,126 +1,137 @@
 
 % Read project results: HDF5
 
-% Tests possible = [9, 10, 11, 11.1 12, 13];
 ExtrVisData = true;
-test = 13;
-
-model_all = {'crhm',...
-            'summa'};
-        
-model_i = 2;
-
 outputFolder = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/synthTestT_results';
 
-model_name = model_all{model_i};
+% Runs to process
+model_all = {'crhm',...
+            'summa'};
+tests_all = [9, 10, 11, 11.1 12, 13];
 
-if model_i == 1
-    % crhm
-    results_dir = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/code_crhm/bin/Case_Studies/synthetic_tests';  
-    comptName = 'SOIL_RECHR';
-elseif model_i ==2
-    % summa
-    results_dir = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/Summa-openWQ/bin/synthetic_tests';
-    comptName = 'SCALARAQUIFER';
-end
+%%%%%%%%%%%%%%%%%%%
+% Start Processing
+%%%%%%%%%%%%%%%%%%%
 
-if test == 1
+% Loop over models
+for model_i = 1:numel(model_all)
+
+    model_name = model_all{model_i};
     
-    Synthetic_test = '1_conserv_instant_SW';
+    if model_i == 1
+            % crhm
+            results_dir = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/code_crhm/bin/Case_Studies/synthetic_tests';  
+            comptName = 'SOIL_RECHR';
+    elseif model_i ==2
+        % summa
+        results_dir = '/Users/diogocosta/Library/CloudStorage/OneDrive-impactblue-scientific.com/6_Projects/1_GWF/2_WIP/code/Summa-openWQ/bin/synthetic_tests';
+        comptName = 'SCALARAQUIFER';
+    end
     
-    extractElm_info = {...
-        'SWE@SPECIES_A#MG/L',[1,1,1];...
-        ...'RUNOFF@SPECIES_A#MG/L',[1,1,1];...
-        ...'SSR@SPECIES_A#MG/L',[1,1,1];...
-        ...'SD@SPECIES_A#MG/L',[1,1,1];...
-        'SOIL_RECHR@SPECIES_A#MG/L',[1,1,1];...
-        %'SOIL_LOWER@SPECIES_A#MG/L',[1,1,1];...
-        ...'SURFSOIL@SPECIES_A#MG/L',[1,1,1];...
-        ...'GW@SPECIES_A#MG/L',[1,1,1];...
-        'SWE@SPECIES_A#KG',[1,1,1];...
-        %'RUNOFF@SPECIES_A#KG',[1,1,1];...
-        %'SSR@SPECIES_A#KG',[1,1,1];...
-        %'SD@SPECIES_A#KG',[1,1,1];...
-        'SOIL_RECHR@SPECIES_A#KG',[1,1,1];...
-        %'SOIL_LOWER@SPECIES_A#KG',[1,1,1];...
-        %'SURFSOIL@SPECIES_A#KG',[1,1,1];...
-        %'GW@SPECIES_A#KG',[1,1,1];...
-        };
+    % Loop over tests
+    for test_num = 1:numel(tests_all)
 
-elseif test == 9
-    Synthetic_test = '9_batch_singleSp_1storder';
+        test = tests_all(test_num);
+        
+        if test == 1
 
-    extractElm_info = {...
-        strcat(comptName,'@SPECIES_A#MG'),[1,1,1]
-        };
-    
-elseif test == 10
-    Synthetic_test = '10_batch_singleSp_2ndorder';
+            Synthetic_test = '1_conserv_instant_SW';
 
-    extractElm_info = {...
-        strcat(comptName,'@SPECIES_A#G'),[1,1,1]
-        };
-    
-elseif test == 11
-    Synthetic_test = '11_batch_2species';
+            extractElm_info = {...
+                'SWE@SPECIES_A#MG/L',[1,1,1];...
+                ...'RUNOFF@SPECIES_A#MG/L',[1,1,1];...
+                ...'SSR@SPECIES_A#MG/L',[1,1,1];...
+                ...'SD@SPECIES_A#MG/L',[1,1,1];...
+                'SOIL_RECHR@SPECIES_A#MG/L',[1,1,1];...
+                %'SOIL_LOWER@SPECIES_A#MG/L',[1,1,1];...
+                ...'SURFSOIL@SPECIES_A#MG/L',[1,1,1];...
+                ...'GW@SPECIES_A#MG/L',[1,1,1];...
+                'SWE@SPECIES_A#KG',[1,1,1];...
+                %'RUNOFF@SPECIES_A#KG',[1,1,1];...
+                %'SSR@SPECIES_A#KG',[1,1,1];...
+                %'SD@SPECIES_A#KG',[1,1,1];...
+                'SOIL_RECHR@SPECIES_A#KG',[1,1,1];...
+                %'SOIL_LOWER@SPECIES_A#KG',[1,1,1];...
+                %'SURFSOIL@SPECIES_A#KG',[1,1,1];...
+                %'GW@SPECIES_A#KG',[1,1,1];...
+                };
 
-    extractElm_info = {...
-        strcat(comptName,'@SPECIES_A#MG'),[1,1,1];...
-        strcat(comptName,'@SPECIES_B#MG'),[1,1,1];...
-        };
-    
-elseif test == 11.1
-    Synthetic_test = '11_1_batch_3species';
+        elseif test == 9
+            Synthetic_test = '9_batch_singleSp_1storder';
 
-    extractElm_info = {...
-        strcat(comptName,'@SPECIES_A#MG'),[1,1,1];...
-        strcat(comptName,'@SPECIES_B#MG'),[1,1,1];...
-        strcat(comptName,'@SPECIES_C#MG'),[1,1,1];...
-        };
+            extractElm_info = {...
+                strcat(comptName,'@SPECIES_A#MG'),[1,1,1]
+                };
 
-elseif test == 12
-    Synthetic_test = '12_batch_nitrogencycle';
+        elseif test == 10
+            Synthetic_test = '10_batch_singleSp_2ndorder';
 
-    extractElm_info = {...
-        strcat(comptName,'@Nref#MG'),[1,1,1];...
-        strcat(comptName,'@Nlab#MG'),[1,1,1];...
-        strcat(comptName,'@DON#MG'),[1,1,1];...
-        strcat(comptName,'@DIN#MG'),[1,1,1]
-        };    
-    
-elseif test == 13
-    Synthetic_test = '13_batch_oxygenBODcycle';
+            extractElm_info = {...
+                strcat(comptName,'@SPECIES_A#G'),[1,1,1]
+                };
 
-    extractElm_info = {...
-        strcat(comptName,'@BOD#MG'),[1,1,1];...
-        strcat(comptName,'@DEFICIT_OXYG#MG'),[1,1,1];...
-        strcat(comptName,'@DO#MG'),[1,1,1]
-        };
-end
+        elseif test == 11
+            Synthetic_test = '11_batch_2species';
+
+            extractElm_info = {...
+                strcat(comptName,'@SPECIES_A#MG'),[1,1,1];...
+                strcat(comptName,'@SPECIES_B#MG'),[1,1,1];...
+                };
+
+        elseif test == 11.1
+            Synthetic_test = '11_1_batch_3species';
+
+            extractElm_info = {...
+                strcat(comptName,'@SPECIES_A#MG'),[1,1,1];...
+                strcat(comptName,'@SPECIES_B#MG'),[1,1,1];...
+                strcat(comptName,'@SPECIES_C#MG'),[1,1,1];...
+                };
+
+        elseif test == 12
+            Synthetic_test = '12_batch_nitrogencycle';
+
+            extractElm_info = {...
+                strcat(comptName,'@Nref#MG'),[1,1,1];...
+                strcat(comptName,'@Nlab#MG'),[1,1,1];...
+                strcat(comptName,'@DON#MG'),[1,1,1];...
+                strcat(comptName,'@DIN#MG'),[1,1,1]
+                };    
+
+        elseif test == 13
+            Synthetic_test = '13_batch_oxygenBODcycle';
+
+            extractElm_info = {...
+                strcat(comptName,'@BOD#MG'),[1,1,1];...
+                strcat(comptName,'@DEFICIT_OXYG#MG'),[1,1,1];...
+                strcat(comptName,'@DO#MG'),[1,1,1]
+                };
+        end
 
 
-%% ================================================================================================
-    % DON'T CHANGE BELOW ==============================================================================
-    % ================================================================================================
-if ExtrVisData == true
-   
+        %% ================================================================================================
+            % DON'T CHANGE BELOW ==============================================================================
+            % ================================================================================================
+        if ExtrVisData == true
 
-    openwq_readfuncs_dir = '../../build/source/openwq/openwq/supporting_scripts/Read_Outputs/';
-    addpath(openwq_readfuncs_dir)
 
-    plot_elemt_flag = true;
+            openwq_readfuncs_dir = '../../build/source/openwq/openwq/supporting_scripts/Read_Outputs/';
+            addpath(openwq_readfuncs_dir)
 
-    folderpath = strcat(results_dir, '/', Synthetic_test,'/', model_name,'/Output_OpenWQ/');
+            plot_elemt_flag = true;
 
-    output_openwq_tscollect_all = read_OpenWQ_outputs(...
-        openwq_readfuncs_dir,...    % Fullpath for needed functions
-        folderpath,...              % Provide fullpath to directory where the HDF5 files are located
-        plot_elemt_flag,...         % Flag to specify if to plot variables
-        extractElm_info,...         % If the flag above is 1, then provide details about the variables to plot
-        'HDF5',...                  % Output format
-        true);    % Debug mode
+            folderpath = strcat(results_dir, '/', Synthetic_test,'/', model_name,'/Output_OpenWQ/');
 
-    % Save Results
-    save(strcat(outputFolder,'/',model_name, '/', Synthetic_test,'.mat'), 'output_openwq_tscollect_all');
-    
+            output_openwq_tscollect_all = read_OpenWQ_outputs(...
+                openwq_readfuncs_dir,...    % Fullpath for needed functions
+                folderpath,...              % Provide fullpath to directory where the HDF5 files are located
+                plot_elemt_flag,...         % Flag to specify if to plot variables
+                extractElm_info,...         % If the flag above is 1, then provide details about the variables to plot
+                'HDF5',...                  % Output format
+                true);    % Debug mode
+
+            % Save Results
+            save(strcat(outputFolder,'/',model_name, '/', Synthetic_test,'.mat'), 'output_openwq_tscollect_all');
+
+        end
+    end
 end
