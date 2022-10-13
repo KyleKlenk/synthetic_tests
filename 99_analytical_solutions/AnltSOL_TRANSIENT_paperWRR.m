4ef84aa1a81a13bb3896f39d22d968720633b434
Diogo Costa <diogo.costa@usask.ca>
2022-10-13 07:42:53 +0100

    generalizing analytical solution script to run the different test in an automated way

--- 99_analytical_solutions/AnltSOL_TRANSIENT_paperWRR.m
+++ 99_analytical_solutions/AnltSOL_TRANSIENT_paperWRR.m
@@ -2,91 +2,39 @@
 % 2-dimensional analytical solution - Transient (using the analytical solution for
 % instantaneous point source integrated with respect to time)
 
-function NumVSAnalytical
+function AnltSOL_TRANSIENT_paperWRR
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % INPUT DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+test = 8; % 2, 4, 6, 8, 
+
 % General %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-nx = 12;
 ny = 600;
-media ='SW'; %GW or SW
-
-% Analytical %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-mLayerDepth_summa_m         = 0.006;                        % m
-hru_area_m2                 = 32700;                        % m2
-
-%M       = 350;        % load; g
-mLayerVolFracWat_summa_m3 = 39.376154283055975;
-%UpLayVol = mLayerVolFracWat_summa_m3;
 C0 = 2;
-n       = 1;       	% porosity 
-Dx      = 0.0006;       	% longiitudinal dispersivity - y-direction m2/s
+mLayerDepth_summa_m         = 0.006;                        % m
 Dy      = 0.0001;      	% longiitudinal dispersivity - y-direction m2/s
-r       = 0;       	% reaction rate
-%r       = 0.01/(60*60*24);       	% reaction rate
-Xc      = 175;     	% injection location loation
-Yc      = 1;    	% injection location loation
-%tsim    = 24*24*60;    	% time after injection - units: days
-Zgw     = mLayerDepth_summa_m;        % aquifer thinkness
-specificYield = 0.22;
-iLayerLiqFluxSoil_summa_m_s = 1.0359700931174889e-05 / 0.25 ;       % m/s
+specificYield = 0.25;
+iLayerLiqFluxSoil_summa_m_s = 1.0359700931174889e-05 / specificYield;       % m/s
 %iLayerLiqFluxSoil_summa_m3  = 0.30488599840555514;         % m3/s
-
-%data_step                   = 900;                          % Seconds
-
-V = iLayerLiqFluxSoil_summa_m_s;	% flow velocity (m/day) (average intersticial velocity)
-%V = 0.6;
-%V = -iLayerLiqFluxSoil_summa_m3/data_step * (60*60*24)/hru_area_m2;	% flow velocity (m/day) (average intersticial velocity)
+V = iLayerLiqFluxSoil_summa_m_s;
+%L = V * 60 * 60 * 24 * 20 / 1000;
 
 tsim = [...
         ...0,...
-        %60 * 60,...
-        %60 * 60 * 2,...
-        %60 * 60 * 5,...
-        %60 * 60 * 10,...
-        ...60 * 60 * 24,...
-        %60 * 60 * 24 * 3,...
-        %60 * 60 * 24 * 8,...
         60 * 60 * 24 * 15,...
         60 * 60 * 24 * 70,...
         60 * 60 * 24 * 120,...
         ];
 
-    
-V = iLayerLiqFluxSoil_summa_m_s
-V = 5.1476e-08 / 1000;
-%Dy = 0.06
-C0 = 2;
-%tsim = [2.5, 5, 10, 15, 20];
 
-VolLayer = 39.376154283074612;   % m3
-wflux_s2r = 0.30488599840048847; % m3/s
-
-V = 6 / (VolLayer / (wflux_s2r / 900));
-V = iLayerLiqFluxSoil_summa_m_s
-L = V * 60 * 60 * 24 * 20 / 1000;
-
-% Numerical %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-t_simNUMERICAL = 2000; % time step to load from the model results (to compare the analytical solution against)
-address_sim = strcat('D:\FCL_SEC\Models\S2R2\Playground\Visualization_VisIT\1_S2R2_direct_results\',media,'\',int2str(t_simNUMERICAL),'_',media,'.csv');
-    
 % Storing %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 storage.C0 = C0;
-storage.n = n;
-storage.Dx = Dx;
 storage.Dy = Dy;
-storage.r = r;
-storage.Xc = Xc;
-storage.Yc = Yc;
 storage.tsim = tsim;
-storage.Zgw = Zgw;
 storage.V = V;
-storage.nx = nx;
 storage.ny = ny;
-storage.address_sim = address_sim;
-save InputData storage
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 n_ti = numel(tsim);
@@ -94,7 +42,20 @@ for ti = 1:n_ti
     
     tsim_i = tsim(ti);
     
-    Analytical_calc_1D_contS(tsim_i)
+    if test == 4 || test == 8 
+        
+        % Set reation rate
+        if test == 4; r = 0; end                % conservative
+        if test == 8; r = 0.01/(60*60*24); end  % linear decay
+        storage.r = r;
+        save InputData storage
+        
+        % Call analytical solver
+        Analytical_calc_1D_contS(tsim_i)
+        
+    elseif test ==2 || test == 6
+        
+    end
     %Analytical_calc_1D_2(tsim_i)
     %Analytical_calc_1D_3(tsim_i)
     %Analytical_calc_1D_justAdv(tsim_i, mLayerDepth_summa_m)
@@ -395,50 +356,8 @@ storage.C_analytical = Cfinal';
 save AnalytSOL storage;
 
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% NUMERICAL
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function Numerical_calc
-
-load InputData storage
-nx=storage.nx;
-ny=storage.ny;
-address_sim=storage.address_sim;
-
-A=importdata(address_sim);
-Adata=A.data;
-
-% Transforming into a matricial form (MODFLOW grid convention)
-c_num=zeros(ny,nx);
-h=waitbar(0,'NUMERICAL Solution: Importing and transforming...');
-for j=1:ny
-     waitbar(j/ny)
-    for i=1:nx
-        l=find(Adata(:,2)==i & Adata(:,3)==j);
-        c_num(j,i)=Adata(l,4);
-    end
-end
-close(h)
-
-disp('NUMERICAL solution loading and transformation: OK')
-
-storage.C_numerical=c_num;
-save NumericalSOL storage;
-
-%figure
-%contour(c_num,'ShowText','on');
-%colormap
-%colorbar('location','Eastoutside')
-%grid on
-%xlabel('Distance along the x-direction')
-%ylabel('Distance along the y-direction')
-%legend('Numerical Solution')
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% COMPARISON
+% Plotting
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function Compare_plot(ti, n_ti, tsim_i,mLayerDepth_summa_m)
@@ -449,8 +368,6 @@ InputData = storage;
 load AnalytSOL storage
 C_analytical = storage.C_analytical;
 
-%load NumericalSOL storage
-%C_numerical = storage.C_numerical;
 
 colormap(gray)
 cmap = colormap
@@ -464,24 +381,6 @@ nsubplot_y = ceil(n_ti / nsubplot_x);
 if ti==1
      figure
 end
-%subplot(3,3,[1,2,4,5])
-%[~, hh]=contour(C_analytical);
-%set(gca, 'Ydir', 'reverse'); 
-%set(hh,'LineColor','k','ShowText','on')
-%grid on
-%hold on
-
-%{
-pcolor(C_numerical)%,'ShowText','on');
-shading flat
-colormap(cmap)
-grid on
-%}
-
-%colorbar('location','East')
-%xlabel('Distance along the x-direction')
-%ylabel('Distance along the y-direction')
-%legend('Analytical','Numerical')
 
 % Longitudinal profile
 subplot(nsubplot_x,nsubplot_y,ti)
@@ -496,14 +395,6 @@ legend('Analytical Solution')
 set (gca,'Xdir','reverse')
 grid on
 
-% Transverse profile
-%subplot(3,3,[7,8])
-%plot(0:1:InputData.nx-1, C_analytical(round(InputData.Yc),:),'k','linewidth',1)
-%xlim([1 InputData.nx])
-%legend('Analytical Solution')
-%ylabel('Concentration')
-%xlabel('Distance along the x-direction')
-%grid on
 
 disp('ANALYTICAL and NUMERICAL solutions compared: OK')
 disp('Finished')
